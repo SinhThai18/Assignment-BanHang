9e121f28d7b732e9846beacc53e605fef989d3c9
SinhThai18 <156230586+SinhThai18@users.noreply.github.com>
2024-03-03 18:12:25 +0700

    day1

---
+++ QLSP.sql
@@ -0,0 +1,32 @@
+﻿CREATE DATABASE QLSP
+USE QLSP
+
+-- Tạo bảng Category
+CREATE TABLE Category (
+    cid INT PRIMARY KEY,
+    cname NVARCHAR(255)
+);
+
+-- Tạo bảng Account
+CREATE TABLE Account (
+    uID INT PRIMARY KEY,
+    username VARCHAR(255),
+    password VARCHAR(255),
+    isSell BIT,
+    isAdmin BIT
+);
+
+-- Tạo bảng Product
+CREATE TABLE Product (
+    id INT PRIMARY KEY,
+    name NVARCHAR(255),
+    image VARCHAR(255),
+    price DECIMAL(10, 2),
+    description TEXT,
+    cateID INT,
+    sell_id INT,
+    FOREIGN KEY (cateID) REFERENCES Category(cid),
+    FOREIGN KEY (sell_id) REFERENCES Account(uID)
+);
+
+select*from Product
